a531dedbeb9d92bb56ce1fbcc6ad258fbf885217
Cole <propertyisrobbery@hotmail.com>
2022-06-14 17:24:49 -0400

    start

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Etch-a-Sketch
 Odin project Etch a Sketch: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+Personal goals for this project: 
+learn how to use CSS grid and to achieve the same design with flexbox. 
+learn more about changing HTML with javascript. 
---
+++ script.js
@@ -0,0 +1,11 @@
+const gridContainer = document.getElementsByClassName("gridContainer");
+//let gridSize = prompt("how many rows/columns?");
+function createGridSize(gridSize){
+}
+function createGridCells() {
+  let cell = document.createElement("div");
+  for(let i = 0; i < cell; i++){
+    appendChild(cell);
+    cell.style.border= "1px black"
+  }
+}
