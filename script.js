21d98d1c5752449cee37935ee02bb290d8bfe79c
Cole <propertyisrobbery@hotmail.com>
2022-06-14 18:30:27 -0400

    create gridSize function

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const gridContainer = document.getElementsByClassName("gridContainer");
-//let gridSize = prompt("how many rows/columns?");
-function createGridSize(gridSize){
+
+function createGridSize(gridSize) {
+  gridSize = ~~prompt("how many rows/columns?");
+  console.log(gridSize);
+  if (isNaN(gridSize) || gridSize < 1 || gridSize > 250) {
+    alert("please enter a positive whole number integer less than 250");
+    return createGridSize();
+  }
+  console.log(typeof gridSize);
+  console.log(gridSize);
+  return gridSize;
 }
-function createGridCells() {
-  let cell = document.createElement("div");
-  for(let i = 0; i < cell; i++){
-    appendChild(cell);
-    cell.style.border= "1px black"
+
+function createGridCells(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    const cell = document.createElement("div");
+    gridContainer.appendChild(cell);
+    cell.style.border = "1px solid black";
   }
 }
+// calling functions
+createGridSize();
+createGridCells();
