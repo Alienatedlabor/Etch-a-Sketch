34147212c48d94c7aa08108fabc282c2a04e12a0
Cole <propertyisrobbery@hotmail.com>
2022-06-14 20:22:18 -0400

    add grid generation functionality

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-const gridContainer = document.getElementsByClassName("gridContainer");
-
-function createGridSize(gridSize) {
-  gridSize = ~~prompt("how many rows/columns?");
-  console.log(gridSize);
-  if (isNaN(gridSize) || gridSize < 1 || gridSize > 250) {
-    alert("please enter a positive whole number integer less than 250");
-    return createGridSize();
-  }
-  console.log(typeof gridSize);
-  console.log(gridSize);
-  return gridSize;
-}
+const gridContainer = document.querySelector(".gridContainer");
 
 function createGridCells(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
-    const cell = document.createElement("div");
-    gridContainer.appendChild(cell);
-    cell.style.border = "1px solid black";
+  gridSize = ~~prompt("How many rows/columns?");
+  console.log(gridSize);
+  if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert("Please enter a positive whole number integer less than 250");
+    return createGridCells();
   }
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell")
+    gridContainer.appendChild(cell);
+  }
+  
+  
 }
 // calling functions
-createGridSize();
 createGridCells();
